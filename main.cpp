f10bec8c3b4d15f2e6e2b2934b9b91b2fba51e08
GerardoAdrian74 <gaeschlimann700@gmail.com>
2024-06-06 20:34:38 -0600

    cout

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <SDL.h>
 #include <stdio.h>
-
+#include <iostream>
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[]) {
+	std::cout<< "hola";
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
 

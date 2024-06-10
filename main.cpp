a3ffccfc57dad6e65730555bfea259836c2f1725
GerardoAdrian74 <gaeschlimann700@gmail.com>
2024-06-10 08:41:42 -0600

    Agregar Variable

--- main.cpp
+++ main.cpp
@@ -17,91 +17,145 @@ struct Constante {
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+Constante parsearExpresion(string expresion) {
+	Constante Exp_Parseado;
+	bool claveLeida = false;
+	string  tmpclave = "", tmpValor = "";
+	for (int i = 0; i < expresion.size(); i++) {
+		if (expresion[i] != ' ') {
+			if (claveLeida == true) {
+				tmpValor += expresion[i];
+			}
+			if (expresion[i] == '=') {
+				claveLeida = true;
+			}
+			if (claveLeida == false) {
+				tmpclave += expresion[i];
+			}
+		}
+
+	}
+	Exp_Parseado.clave = tmpclave;
+	Exp_Parseado.valor = stof(tmpValor);
+
+	return Exp_Parseado;
+}
 vector<Constante> CargarConstantes() {
 	ifstream archivo;
+	vector<Constante> arr_Constantes;
 	archivo.open("Constantes.txt", ios::in);
 	if (archivo.fail()) {
-		cout << "Error abriendo el archivo de constantes\n";
 		archivo.close();
+		return arr_Constantes;
 	}
-	vector<Constante> arr_Constantes;
+	
 	
 	string linea;
 	
 	while (getline(archivo, linea)) {
-		bool claveLeida = false;
-		string  tmpclave = "", tmpValor = "";
-		for (int i = 0; i < linea.size(); i++) {
-			if (linea[i] != ' ') {
-				if (claveLeida == true) {
-					tmpValor += linea[i];
-				}
-				if (linea[i] == '=') {
-					claveLeida = true;
-				}
-				if (claveLeida == false) {
-					tmpclave += linea[i];
-				}
-			}
-
-		}
-		Constante constante;
-		constante.clave = tmpclave;
-		constante.valor = stof(tmpValor);
+		Constante constante=parsearExpresion(linea);
 		arr_Constantes.push_back(constante);
 
 	}
 	archivo.close();
-
 	return arr_Constantes;
 }
+vector<Constante> CargarVariables() {
+	ifstream archivo;
+	vector<Constante> arr_variables;
+	archivo.open("Variables.txt", ios::in);
+	if (archivo.fail()) {
+		archivo.close();
+		return arr_variables;
+	}
+	string linea;
+	while (getline(archivo, linea)) {
+		Constante constante = parsearExpresion(linea);
+		arr_variables.push_back(constante);
 
+	}
+	archivo.close();
+	return arr_variables;
+}
+
+bool agregarVariable(vector<Constante> ListaConstantes, vector<Constante> ListaVariables,string expresion) {
+		ifstream leer;
+		ofstream escribir;
+
+		leer.open("Variables.txt");
+		if (leer.fail()) {
+			leer.close();
+			escribir.open("Variables.txt",ios::out);
+			
+		}
+		else {
+			escribir.open("Variables.txt", ios::app);
+		}	
+		//Parsear expresion
+		Constante ExpresionParseada=parsearExpresion(expresion);
+		//Comparacion Constantes
+		for (Constante tmp : ListaConstantes) {
+			if (ExpresionParseada.clave==tmp.clave) {
+				return false;
+			}
+		}
+		//Comparacion Variables
+		for (Constante tmp : ListaVariables) {
+			if (ExpresionParseada.clave == tmp.clave) {
+				return false;
+			}
+		}
+		escribir <<expresion<<endl;
+		escribir.close();
+		
+	return true;
+}
 int main(int argc, char* args[]) {
 	ifstream archivo;
-	archivo.open("Constantes.txt", ios::in);
-	if (archivo.fail()) {
-		cout << "Error abriendo el archivo de constantes\n";
-		archivo.close();
-		return 1;
+	vector<Constante>  Lista_Constantes = CargarConstantes();
+	vector<Constante> Lista_Variables = CargarVariables();
+	string expresion;
+	cout << "Ingresa una expresion para una variable: ";
+	cin >> expresion;
+	if(agregarVariable(Lista_Constantes, Lista_Variables, expresion)) {
+		cout << "Se agrego correctamente\n";
 	}
-	vector<Constante> Lista_Constantes= CargarConstantes();
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	else {
+		cout << "Error en la variable\n";
+	}
 
-	//The surface contained by the window
+	//SDL
+	SDL_Window* window = nullptr;
 	SDL_Surface* screenSurface = NULL;
 
-	//Initialize SDL
+	//Inicializar SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
 	else {
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		//Crear ventana
+		window = SDL_CreateWindow("Evaluador de Expresiones", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
 		else {
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+			bool running = true;
+			while (running) {
+				SDL_Event event;
+				while (SDL_PollEvent(&event)) {
+					if (event.type == SDL_QUIT) {
+						running = false;
+					}
+				}
+			}
 		}
+
+		//Salir de la ventana
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+		return 0;
 	}
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
 }
